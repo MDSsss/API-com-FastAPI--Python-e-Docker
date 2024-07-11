5986930cd4023e440f9fceb4e0286fd41a85f585
MDSsss <Mauricio_domingo.09@hotmail.com>
2024-07-11 20:35:46 -0300

    Final commit

---
+++ workout_api/contrib/dependencies.py
@@ -0,0 +1,6 @@
+from typing import Annotated
+from fastapi import Depends
+from workout_api.configs.database import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+DatabaseDependency = Annotated[AsyncSession, Depends(get_session)]
